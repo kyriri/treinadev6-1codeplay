7e1820c7c38c3f509a91a1975936f1aa57d73e06
kyriri <git-kyriri@opayq.com>
2021-05-14 16:08:55 -0300

    Restrict available routes to those actually used

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :courses
+  resources :courses, only: [:index, :show] # only limits the available routes
 end
