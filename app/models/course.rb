dbb7e31756cf778db81416b2d9a02a842ba9ce9b
kyriri <git-kyriri@opayq.com>
2021-05-16 18:56:00 -0300

    Validate Course attributes on model level

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ApplicationRecord
+  validates :name, 
+            :description,
+            :code,
+            :price,
+            :enrollment_deadline,
+            presence: { message: "não pode ficar em branco" }
+  validates :code, uniqueness: { message: "já está em uso" }
 end
