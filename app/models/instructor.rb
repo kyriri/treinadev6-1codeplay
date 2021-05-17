669dd7d7c19ecc8a9eeb24110e48818143ba8813
kyriri <git-kyriri@opayq.com>
2021-05-17 13:34:17 -0300

    Admin can upload Instructor profile picture

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,7 @@
 class Instructor < ApplicationRecord
-    validates :name, :email,
-              presence: { message: "não pode ficar em branco" }
+  has_one_attached :profile_picture
+
+  validates :name, :email,
+            presence: { message: "não pode ficar em branco" }
   validates :email, uniqueness: { message: "já cadastrado em outro registro" }
 end
