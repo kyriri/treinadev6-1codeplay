55b0d68f6bae9d96fb5596557c6daea3b3606f12
kyriri <git-kyriri@opayq.com>
2021-06-05 18:29:16 -0300

    Create scopes for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,14 @@ class Course < ApplicationRecord
             :price,
             presence: { message: "não pode ficar em branco" }
   validates :code, uniqueness: { message: "já está em uso" }
+
+  # scopes can be seen as custom query filters, can be chained
+  scope :available, -> { where(enrollment_deadline: Date.current..) } #, draft: false }
+  scope :price_asc, -> { order(price: :asc) } # or :desc
+
+  # scope is the same as:
+  # def self.available
+  #   where(enrollment_deadline: Date.current..)
+  # end
+  # however, a method might not be chainable
 end
